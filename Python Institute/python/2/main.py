1dbc8222e80adbe3fafa7977c2000115b995c042
engineeredcurlz <32712461+engineeredcurlz@users.noreply.github.com>
2026-01-26 17:31:42 -0500

    mem heavy

--- Python Institute/python/2/main.py
+++ Python Institute/python/2/main.py
@@ -5,3 +5,7 @@ import itertools
 with open('large_file.csv' , 'w') as f:
     for i in range(1000000):
         f.write(f"{i},data_{i}\n")
+
+# memory heave approach
+lines_list = [line.strip().upper for line in open('large_file.csv')]
+print(f"List comprehension memory usage: {sys.getsizeof(lines_list)} bytes")
